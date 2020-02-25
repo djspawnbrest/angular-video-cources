6d74a6e10b2127533c5e7ad276855120a569cf3d
djspawnbrest <djspawnbrest@gmail.com>
2020-02-25 12:49:57 +0300

    refactor class CourseItem

--- src/app/course-list/models/course-item.ts
+++ src/app/course-list/models/course-item.ts
@@ -2,20 +2,11 @@ import {CourseItemInterface} from './course-item.model';
 
 // TODO: generate id and set creationDate in condtructor
 export class CourseItem implements CourseItemInterface {
-    id: number;
-    title: string;
-    author: string;
     creationDate: string;
     duration: Number;
-    description: string;
     toTime: string;
 
-    constructor(id: number, title: string, author: string, description: string, duration?: Number, creationDate?: string) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.description = description;
-        this.duration = duration;
+    constructor(public id: number, public title: string, public author: string, public description: string, duration?: Number, creationDate?: string, toTime?: string) {
         this.creationDate = creationDate;
         this.toTime = this.timeConvert(duration);
     }
