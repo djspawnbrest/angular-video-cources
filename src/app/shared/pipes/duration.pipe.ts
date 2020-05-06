2b126d889059d3a31af744b2f061f0a09cea2afa
djspawnbrest <djspawnbrest@gmail.com>
2020-05-06 03:31:04 +0300

    clean code

--- src/app/shared/pipes/duration.pipe.ts
+++ src/app/shared/pipes/duration.pipe.ts
@@ -1,17 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { AsyncPipe } from '@angular/common';
-import { map } from 'rxjs/operators';
 
 @Pipe({
   name: 'duration'
 })
 export class DurationPipe implements PipeTransform {
 
-  public constructor(
-    private translate: TranslateService,
-    private readonly async: AsyncPipe
-    ) {}
+  public constructor() {}
 
   transform(duration: number): any {
     const hour = Math.floor(duration / 60);
@@ -24,24 +18,5 @@ export class DurationPipe implements PipeTransform {
     }
     return result;
   }
-
-  // transform(duration: number): string {
-  //   const hour = Math.floor(duration / 60);
-  //   const min = duration % 60;
-  //   let result = '';
-  //   const h = this.async.transform(this.translate.get('ADDEDIT.DURATIONH', {value: hour}).pipe(map( (x: string) => {
-  //     return x;
-  //   })));
-  //   const m = this.async.transform(this.translate.get('ADDEDIT.DURATIONM', {value: min}).pipe(map( (x: string) => {
-  //     return x;
-  //   })));
-  //   if (hour >= 1)  {
-  //     result += `${h}`;
-  //   }
-  //   if (min > 0) {
-  //     result += ` ${m}`;
-  //   }
-  //   return result;
-  // }
 
 }
